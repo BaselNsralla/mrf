c9089725b321f466a6b440b9db84bbfa6864b120
Basel <basel.obeid9661@gmail.com>
2018-03-14 17:47:06 +0100

    Added README and example file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Make React file
+
+### Usage
+Navigate to the directory where you want to create a react-native file the run:
+
+```console 
+node mrf <arg1> p? 
+```
+
+* arg1 is the name of the class you want to create which will be the name of the file
+* `p` is optional and will send props to the constructor if you write.
+
+
+### Example
+in `Sick.js`
+
---
+++ Sick.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AsyncStorage, TouchableOpacity, Dimensions, Image } from 'react-native';
+
+export default class Sick extends Component {
+  constructor(props) {
+    super(props)
+    this.state={ }
+  }
+
+  render() {
+    return(
+      <View style={{}}>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+
+})
